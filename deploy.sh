8e9565a3f44051da03ef859b5ce0b8cf435b61e8
Emma Power <power.emma@outlook.com>
2026-02-18 13:25:41 -0500

    fix trailing api /

--- deploy.sh
+++ deploy.sh
@@ -255,7 +255,7 @@ server {
   error_log  /var/log/nginx/os9_phoenix.error.log warn;
 
   location /api/ {
-    proxy_pass http://127.0.0.1:3000/;
+    proxy_pass http://127.0.0.1:3000;
     proxy_http_version 1.1;
     proxy_set_header Host \$host;
     proxy_set_header X-Real-IP \$remote_addr;
